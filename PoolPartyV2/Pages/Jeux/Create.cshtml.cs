15e860eea9a600e51dc719b143b227f642c49687
Maxime <maxime.lievin@etud.u-picardie.fr>
2021-05-20 18:49:37 +0200

    Ajout des pages Razo + auth + rôles

--- PoolPartyV2/Pages/Jeux/Create.cshtml.cs
+++ PoolPartyV2/Pages/Jeux/Create.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -10,6 +11,7 @@ using PoolPartyV2.Models;
 
 namespace PoolPartyV2.Pages.Jeux
 {
+    [Authorize]
     public class CreateModel : PageModel
     {
         private readonly PoolPartyV2.Data.ApplicationDbContext _context;
